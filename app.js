5c69360f826a57c6270c45d58b0ed802f4c4e569
Valeria Lopez <vplopezm@gmail.com>
2023-02-22 18:13:01 -0300

    new commit - localstorage and dark/light mode

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-function toggleColorMode() {
-  let navToggleColor = document.getElementById("toggle");
-  let currentClass = navToggleColor.className;
-  navToggleColor.className =
-    currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-}
+const theme = document.querySelector("#theme");
+const toggle = document.querySelector("#toggle");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTheme();
+});
+
+// recuperar el mode del localStorage
+const setTheme = () => {
+  const modeState = localStorage.getItem("mode");
+  if (modeState && modeState === "light-mode") {
+    theme.className = modeState;
+    toggle.checked = true;
+  } else {
+    theme.className = "dark-mode";
+    toggle.checked = false;
+  }
+};
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    localStorage.setItem("mode", "light-mode");
+    theme.className = "light-mode";
+  } else {
+    localStorage.setItem("mode", "dark-mode");
+    theme.className = "dark-mode";
+  }
+});
